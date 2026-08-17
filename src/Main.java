5b9de90b0484fc6479514fececc1d674923a95d0
AzounV <andres.azabache@outlook.com>
2026-08-18 09:39:56 +1000

    Created window

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.awt.*;
+import java.awt.event.*;
 public class Main {
     public static void main(String [] args) {
+        Frame frame = new Frame();
         System.out.println("Hello, world! but sheep");
+
+        frame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e){
+                System.exit(0);
+            }
+        });
+
+        frame.setSize(640, 480);
+        frame.setTitle("Sheep Simulation");
+        frame.setBackground(Color.BLACK);
+        frame.setLayout(null);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
 }
