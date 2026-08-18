ccfcd12e71e6adb92252635c0239b97319d1061d
AzounV <andres.azabache@outlook.com>
2026-08-18 10:24:29 +1000

    Made entity class extend Panel Please enter the commit message for your changes. Lines starting

--- src/Entity.java
+++ src/Entity.java
@@ -1,5 +1,6 @@
-public abstract class Entity{
-
+import java.awt.*;
+public abstract class Entity extends Panel {
+    public Position pos;
 
     public double nutrition = 0.3; //How much this entity fills a creatures hunger when eaten
 
@@ -7,5 +8,15 @@ public abstract class Entity{
     public void KillEntity()
     {
         //Death logic
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        g.setColor(Color.green);
+        g.fillRect(pos.getX(), pos.getY(), 10, 10);
+    }
+
+    public void drawEntity() {
+        repaint();
     }
 }
