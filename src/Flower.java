4825fb861781d9874a9eda48872df88c44a5a206
Amelia <amelia.winn86@gmail.com>
2026-08-18 10:59:30 +1000

    make plant classes

---
+++ src/Flower.java
@@ -0,0 +1,18 @@
+
+import java.awt.Color;
+
+
+
+public abstract class Flower extends Plant {
+        
+    int random = (int) Math.random();
+
+    public Color flowerColour = new Color(random);
+
+
+    public Flower()
+    {
+
+
+    }
+}
