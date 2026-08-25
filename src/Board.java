a2895ebbd423b965e9a95c971cec134b09b89c28
AzounV <andres.azabache@outlook.com>
2026-08-25 10:47:07 +1000

    Added arrayList of Arraylist to hold entities and enum to specify which index

--- src/Board.java
+++ src/Board.java
@@ -8,7 +8,7 @@ public class Board extends Frame implements ActionListener {
     private int bWidth; // Board width
     private int bHeight; // Board height
     private Timer timer;
-    private List<Entity> entities;
+    private List<List<Entity>> entities;
 
     public Board(int bWidth, int bHeight) {
         this.bWidth = bWidth;
@@ -26,13 +26,19 @@ public class Board extends Frame implements ActionListener {
         setBackground(Color.BLACK);
 
         entities = new ArrayList<>();
-        entities.add(new Wolf());
-        entities.add(new Sheep("Mary", 10, 3, 5, new Animal[]{null, null}));
-        entities.add(new Grass());
-        entities.add(new Flower());
-        for (Entity ent : entities) {
-            ent.pos = new Position((int)(Math.random() * (bWidth / 40)) * 40, (int)(Math.random() * (bHeight / 40)) * 40);
-            System.out.println(ent);
+        entities.add(new ArrayList<>());
+        entities.add(new ArrayList<>());
+        entities.add(new ArrayList<>());
+        entities.add(new ArrayList<>());
+        entities.get(Ent.sheep.get()).add(new Sheep("Mary", 10, 3, 5, new Animal[]{null, null}));
+        entities.get(Ent.wolf.get()).add(new Wolf());
+        entities.get(Ent.flower.get()).add(new Flower());
+        entities.get(Ent.grass.get()).add(new Grass());
+        for (List<Entity> list : entities) {
+            for (Entity ent : list) {
+                ent.pos = new Position((int)(Math.random() * (bWidth / 40)) * 40, (int)(Math.random() * (bHeight / 40)) * 40);
+                System.out.println(ent);
+            }
         }
 
         timer = new Timer(10, this);
@@ -41,13 +47,29 @@ public class Board extends Frame implements ActionListener {
     @Override
     public void paint(Graphics g) {
         super.paint(g);
-        for (Entity ent : entities) {
-            ent.drawEntity(g);
+        for (List<Entity> list : entities) {
+            for (Entity ent : list) {
+                ent.drawEntity(g);
+            }
         }
     }
     @Override
     // After the timer finishes do this
     public void actionPerformed(ActionEvent e) {
         repaint();
+    }
+
+    public enum Ent {
+        sheep(0), wolf(1), flower(2), grass(3);
+
+        private final int index;
+        
+        private Ent(int index) {
+            this.index = index;
+        }
+
+        public int get() {
+            return index;
+        }
     }
 }
