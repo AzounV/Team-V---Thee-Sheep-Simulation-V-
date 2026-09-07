78f28097aa9670f1812629f9e08b09c90b36f871
AzounV <andres.azabache@outlook.com>
2026-09-08 09:13:16 +1000

    Added the sheeps movement to the wolf, but with sheep as the target instead of flowers

--- src/Wolf.java
+++ src/Wolf.java
@@ -1,19 +1,43 @@
 import java.awt.*;
+import java.util.List;
 
 public class Wolf extends Animal {
     public Wolf() {
         super("Wolf", 1.5, 3.0, 8, null);
         this.colour = Color.gray;
     }
+    public Wolf(String name, float speed, float perception, int lifeSpan, Animal[] parents)
+    {
+        super(name, speed, perception, lifeSpan, parents);
+        this.colour = Color.gray;
+        entityType = Board.Ent.wolf;
+
+    }  
 
     @Override
-    public void Move() {
+    public Entity LookForFood() {
+        List<Entity> allSheep = Board.entities.get(Board.Ent.sheep.get());
 
-    }
+        Entity closest = null;
 
-    @Override
-    public void LookForFood() {
-        
+        double closestDist = 99999;
+        for (Entity sheep : allSheep) {
+
+            double dist = this.pos.dist(sheep.pos) ;
+            if(dist < this.perception && dist < closestDist)
+            {
+                closest = sheep;
+                closestDist = dist;
+            }
+        }
+
+        if(closest != null)
+        {
+            // System.out.println("Closest sheep @ " + closest.pos);
+        }else{
+            // System.out.println("No sheep in range");
+        }
+        return closest;
     }
 
     
