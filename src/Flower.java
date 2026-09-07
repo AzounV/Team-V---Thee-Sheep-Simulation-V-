fa95a115d3746ebc89bfc976c70c2107e2536c67
AzounV <andres.azabache@outlook.com>
2026-09-08 09:18:26 +1000

    Fixed flower colour randomisation

--- src/Flower.java
+++ src/Flower.java
@@ -5,12 +5,13 @@ public class Flower extends Plant {
     Color fColour;
     public Flower()
     {
-        int randomR = ((int) Math.random())*5;
-        int randomG = ((int) Math.random())*5;
-        int randomB = ((int) Math.random())*5;
+        float randomR = (float)Math.random() * 0.75f + 0.25f;
+        float randomG = (float)Math.random() * 0.5f;
+        float randomB = (float)Math.random() * 0.75f + 0.25f;
 
         fColour = new Color(randomR, randomG, randomB);
         this.name = "Flower";
+        this.pColour = fColour;
         entityType = Board.Ent.flower;
     }
 }
